17756683f40718e1bbda2e64cefdc6ac7325f1ac
mike <mike@zeoalliance.com>
2016-12-20 19:20:29 +0200

    add method execute example

--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"github.com/godbus/dbus"
 	"log"
-	"fmt"
 )
 
 func main() {
@@ -12,13 +11,22 @@ func main() {
 	if err != nil {
 		log.Fatalln(err)
 	}
+
+	obj := conn.Object("org.freedesktop.NetworkManager", "/org/freedesktop/NetworkManager/Settings")
+	var s interface{}
+	err = obj.Call("ListConnections", 0).Store(&s)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	log.Println(s)
+
 	conn.BusObject().Call("org.freedesktop.DBus.AddMatch", 0,
 		"type='signal',path='/org/freedesktop/NetworkManager',member='StateChanged'")
 
 	c := make(chan *dbus.Signal, 10)
 	conn.Signal(c)
 	for v := range c {
-		fmt.Printf("%+v\n\n", v)
+		log.Printf("%+v\n\n", v)
 		// https://developer.gnome.org/NetworkManager/stable/nm-dbus-types.html#NMState
 		if v.Body[0].(uint32) == 70 {
 			log.Println("NM_STATE_CONNECTED_GLOBAL")

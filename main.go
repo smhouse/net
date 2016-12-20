f927772385c0821fc1e61f4ee09def8d7621a6b8
Mike <mike@mihael.me>
2016-12-21 00:28:55 +0200

    start to implement nm api

--- main.go
+++ main.go
@@ -1,44 +1,17 @@
 package main
 
 import (
-	"github.com/godbus/dbus"
+	"github.com/smhouse/net/nm"
 	"log"
 )
 
 func main() {
-
-	conn, err := dbus.SystemBus()
+	devices, err := nm.GetDevices()
 	if err != nil {
 		log.Fatalln(err)
 	}
 
-	obj := conn.Object("org.freedesktop.NetworkManager", "/org/freedesktop/NetworkManager/Settings")
-	var s interface{}
-	err = obj.Call("org.freedesktop.NetworkManager.Settings.ListConnections", 0).Store(&s)
-	if err != nil {
-		log.Fatalln(err)
-	}
-	log.Println(s)
-
-	v, err := obj.GetProperty("org.freedesktop.NetworkManager.Settings.Hostname")
-	if err != nil {
-		log.Fatalln(err)
-	}
-
-	log.Printf("%v", v)
-
-	conn.BusObject().Call("org.freedesktop.DBus.AddMatch", 0,
-		"type='signal',path='/org/freedesktop/NetworkManager',member='StateChanged'")
-
-	c := make(chan *dbus.Signal, 10)
-	conn.Signal(c)
-	for v := range c {
-		log.Printf("%+v\n\n", v)
-		// https://developer.gnome.org/NetworkManager/stable/nm-dbus-types.html#NMState
-		if v.Body[0].(uint32) == 70 {
-			log.Println("NM_STATE_CONNECTED_GLOBAL")
-		} else if v.Body[0].(uint32) == 20 {
-			log.Println("NM_STATE_DISCONNECTED")
-		}
+	for _, d := range *devices {
+		log.Println(d)
 	}
 }
---
+++ nm/device.go
@@ -0,0 +1,23 @@
+package nm
+
+import (
+	"github.com/godbus/dbus"
+)
+
+func GetDevices() (*[]dbus.ObjectPath, error) {
+	conn, err := dbus.SystemBus()
+	if err != nil {
+		return nil, err
+	}
+	defer conn.Close()
+
+	obj := conn.Object("org.freedesktop.NetworkManager", "/org/freedesktop/NetworkManager")
+	var s interface{}
+	err = obj.Call("org.freedesktop.NetworkManager.GetAllDevices", 0).Store(&s)
+	if err != nil {
+		return nil, err
+	}
+
+	ret := s.([]dbus.ObjectPath)
+	return &ret, nil
+}
